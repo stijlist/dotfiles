140144537c38f6a707d7bea97b9265c992e270bc
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-07-20 06:31:35 -0400

    Add karabiner remap script

---
+++ karabiner-import.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.controlL2controlL_escape 1
+/bin/echo -n .
+$cli set option.emacsmode_controlM 1
+/bin/echo -n .
+/bin/echo
