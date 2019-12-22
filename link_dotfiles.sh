22d285add5d42ed74f188877c484434a2cb39291
Bert Muthalaly <stijlist@google.com>
2019-12-21 18:17:51 -0600

    don't link dotvim anymore (unused)

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/dotvim ~/.vim
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/fish
 ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
