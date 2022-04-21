45cb262b2111614fcfd7dff26179100a731bf78b
Bert Muthalaly <bert@somethingdoneright.net>
2022-04-20 23:29:40 -0400

    Make buskill a C program using libusb

--- link_dotfiles_macos.sh
+++ link_dotfiles_macos.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 mkdir -p ~/Library/KeyBindings/ && ln -s ~/dotfiles/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
+ln -s ~/dotfiles/net.somethingdoneright.buskill.plist ~/Library/LaunchAgents/
