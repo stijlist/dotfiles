fef7d8287917e95389e0227aefe805b6973361de
Bert Muthalaly <bert@somethingdoneright.net>
2022-04-22 14:44:14 -0400

    Add hammerspoon_init.lua.
    Currently includes functionality for sending windows to the left and
    right half of the screen (command+alt+left|right), plus a rudimentary
    tiling feature (can tile windows in left and right "stacks" by using
    ctrl+command+alt+left|right).

---
+++ hammerspoon_init.lua
@@ -0,0 +1,145 @@
+local laptopScreen = "Built-in Retina Display"
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.alert.show( #stacks["left"]) -- , #stacks["right"])
+end)
+
+hs.window.animationDuration = 0
+
+hs.hotkey.bind({"cmd", "alt"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  -- if a window is in the other stack, remove it.
+  local other = stacks["right"]
+  if contains(other, win) then
+    table.remove(other, find(win, other))
+    renderStacks()
+  end
+
+  local f = win:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  -- if a window is in the other stack, remove it.
+  local other = stacks["left"]
+  if contains(other, win) then
+    table.remove(other, find(win, other))
+    renderStacks()
+  end
+
+  local f = win:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- "left"|"right" -> index -> win
+stacks = {left={}, right={}}
+
+function renderStacks()
+  for name, s in pairs(stacks) do
+    renderStack(s, name)
+  end
+end
+
+function sendToStack(name, win)
+  -- identify stack and other
+  local stacks = shallowcopy(stacks)
+  local stack = remove(stacks, name)
+  local other = stacks[next(stacks)]
+
+  -- if a window is in the other stack, remove it.
+  if contains(other, win) then
+    table.remove(other, find(win, other))
+  end
+  -- if the window is already in this stack, cycle it.
+  if contains(stack, win) then
+    shift(stack)
+  else
+    -- otherwise, add it to the stack.
+    table.insert(stack, win)
+  end
+  renderStacks()
+end
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  sendToStack("left", win)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  sendToStack("right", win)
+end)
+
+function renderStack(stack, side)
+  local count = #stack
+  for i, win in pairs(stack) do
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+    local leftPad = 0
+    if side == "right" then
+      leftPad = (max.w / 2)
+    end
+    local h = max.h / count
+    f.x = max.x + leftPad
+    f.y = max.y + (h * (i-1))
+    f.w = max.w / 2
+    f.h = h
+    win:setFrame(f)
+  end
+end
+
+function contains(stack, win)
+  for i, v in pairs(stack) do
+    if v == win then
+      return true
+    end
+  end
+  return false
+end
+
+function find(win, stack)
+  for i, w in pairs(stack) do
+    if w == win then
+      return i
+    end
+  end
+  return -1
+end
+
+function shift(stack)
+  first = table.remove(stack, 1)
+  table.insert(stack, first)
+  return stack
+end
+
+function shallowcopy(table)
+  local result = {}
+  for k, v in pairs(table) do
+    result[k] = v
+  end
+  return result
+end
+
+function remove(table, key)
+  v = table[key]
+  table[key] = nil
+  return v
+end
--- link_dotfiles_macos.sh
+++ link_dotfiles_macos.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mkdir -p ~/Library/KeyBindings/ && ln -s ~/dotfiles/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
 ln -s ~/dotfiles/net.somethingdoneright.buskill.plist ~/Library/LaunchAgents/
+ln -s ~/dotfiles/hammerspoon_init.lua ~/.hammerspoon/init.lua
