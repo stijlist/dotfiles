e0ef753768065ef85dbd783a39f5159c4f9c1038
Bert Muthalaly <bert@somethingdoneright.net>
2025-10-29 22:46:51 -0400

    Link .profile in link_dotfiles.sh

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -2,6 +2,7 @@
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/profile ~/.profile
 mkdir -p ~/.config/fish
 ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
 # cp ~fish-history-merge/functions/up-or-search.fish ~/.config/fish/functions/up-or-search.fish
